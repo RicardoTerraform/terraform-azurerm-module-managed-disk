feb76a277bb445756b315b43cbc46b34ddeaae3d
Ricardo Jorge Alves <ricardo.j.alves@noesis.pt>
2023-07-07 09:14:09 +0100

    upload files

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_managed_disk" "example" {
   resource_group_name = data.azurerm_resource_group.rgname
 
   storage_account_type = var.os_storage_account_type
-  disk_size_disk_size_gb =             = split("_", var.os_storage_account_type)[0] != "PremiumV2" ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : var.os_disk_size_gb
+  disk_size_gb         = split("_", var.os_storage_account_type)[0] != "PremiumV2" ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : var.os_disk_size_gb
 
   create_option      = var.creation
   source_uri         = contains(["Import", "ImportSecure"], var.creation) ? var.source_uri : null
@@ -32,28 +32,28 @@ resource "azurerm_managed_disk" "example" {
   dynamic "encryption_settings" {
     for_each = var.disk_encryption_secret != null ? [var.disk_encryption_secret] : []
 
-content {
-    dynamic "disk_encryption_key" {
+    content {
+      dynamic "disk_encryption_key" {
 
-      for_each = var.disk_encryption_secret != null ? [var.disk_encryption_secret] : []
+        for_each = var.disk_encryption_secret != null ? [var.disk_encryption_secret] : []
 
-      content = {
-        source_vault_id = disk_encryption_key.value.source_vault_id
-        secret_url      = disk_encryption_key.value.secret_url
+        content = {
+          source_vault_id = disk_encryption_key.value.source_vault_id
+          secret_url      = disk_encryption_key.value.secret_url
+        }
       }
     }
-}
-content {
-    dynamic "key_encryption_key" {
-      for_each = var.key_encryption_key != null ? ["true"] : []
+    content {
+      dynamic "key_encryption_key" {
+        for_each = var.key_encryption_key != null ? ["true"] : []
 
-      content = {
-        source_vault_id = var.disk_encryption_secret["source_vault_id"]
-        key_url         = var.disk_encryption_secret["key_url"]
+        content = {
+          source_vault_id = var.disk_encryption_secret["source_vault_id"]
+          key_url         = var.disk_encryption_secret["key_url"]
+        }
       }
     }
   }
-  }
-  
+
   tags = merge(local.tags_default, var.tags)
 }
