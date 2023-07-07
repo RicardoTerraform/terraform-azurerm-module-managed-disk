d05c9df0c6efcf1248875e8efae52f4bf7019f66
Ricardo Jorge Alves <ricardo.j.alves@noesis.pt>
2023-07-07 17:22:09 +0100

    upload files

--- data.tf
+++ data.tf
@@ -3,6 +3,8 @@ data "azurerm_resource_group" "rgname" {
 }
 
 data "azurerm_virtual_machine" "virtualmachine" {
+  count               = var.disk_attach ? 1 : 0
+  
   name                = "${var.azure_system_name}-${var.vm_name}-${var.environment}"
   resource_group_name = data.azurerm_resource_group.rgname.name
 }
