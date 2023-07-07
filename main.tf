4dda0c07dc10985415a24ff46c61c815ce16fb35
Ricardo Jorge Alves <ricardo.j.alves@noesis.pt>
2023-07-07 11:11:39 +0100

    upload files

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ resource "azurerm_managed_disk" "manage_disk" {
   resource_group_name = data.azurerm_resource_group.rgname.name
 
   storage_account_type = var.os_storage_account_type
-  disk_size_gb         = split("_", var.os_storage_account_type)[0] != "PremiumV2" ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : var.os_disk_size_gb
+  disk_size_gb         = os_disk_size_gb != null ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : null
+  #disk_size_gb         = split("_", var.os_storage_account_type)[0] != "PremiumV2" ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : var.os_disk_size_gb
 
   create_option      = var.creation
   source_uri         = contains(["Import", "ImportSecure"], var.creation) ? var.source_uri : null
@@ -18,13 +19,6 @@ resource "azurerm_managed_disk" "manage_disk" {
 
   upload_size_bytes = contains(["Upload"], var.creation) ? var.upload_size_bytes : null
 
-  #ONLY settable for "UltraSSD" and "PremiumV2
-  #disk_iops_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_write : null
-  #disk_mbps_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_write : null
-  #disk_iops_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_only : null
-  #disk_mbps_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_only : null
-  #logical_sector_size  = var.logical_sector_size
-
   #edge_zone = ?
 
   os_type = contains(["Import", "ImportSecure", "Copy"], var.os_type) ? var.os_type : null
@@ -33,9 +27,8 @@ resource "azurerm_managed_disk" "manage_disk" {
   zone       = var.vm_avail_zone_id
 
   dynamic "encryption_settings" {
-    for_each = var.disk_encryption_secret != null ? ["true"] : []
+    for_each = var.disk_encryption_secret != null ? ["true"] : var.key_encryption_key != null ? ["true"] : []
     content {
-
 
       dynamic "disk_encryption_key" {
         for_each = var.disk_encryption_secret != null ? ["true"] : []
@@ -57,6 +50,14 @@ resource "azurerm_managed_disk" "manage_disk" {
   }
 
   tags = merge(local.tags_default, var.tags)
+
+    lifecycle {
+
+    precondition {
+      condition     = (var.os_disk_size_gb == null)
+      error_message = "The variable os_disk_size_gb is REQUIRED, only for UltraSSD and PremiumV2 (os_Storage_account_type)"
+    }
+  }
 }
 
 
@@ -69,50 +70,53 @@ resource "azurerm_managed_disk" "manage_disk_ultra_PV2" {
   resource_group_name = data.azurerm_resource_group.rgname.name
 
   storage_account_type = var.os_storage_account_type
-  disk_size_gb         = split("_", var.os_storage_account_type)[0] != "PremiumV2" ? [for size in local.disks_tiers[split("_", var.os_storage_account_type)[0]] : size if size >= var.os_disk_size_gb][0] : var.os_disk_size_gb
+  disk_size_gb         = var.os_disk_size_gb
 
   create_option      = var.creation
-  source_uri         = contains(["Import", "ImportSecure"], var.creation) ? var.source_uri : null
-  source_resource_id = contains(["Copy", "Restore"], var.creation) ? var.source_resource_id : null
-
-  image_reference_id         = contains(["FromImage"], var.creation) ? var.gallery_image_reference_id != null ? null : var.image_reference_id : null
-  gallery_image_reference_id = contains(["FromImage"], var.creation) ? var.image_reference_id != null ? null : var.gallery_image_reference_id : null
-
-  upload_size_bytes = contains(["Upload"], var.creation) ? var.upload_size_bytes : null
 
   #ONLY settable for "UltraSSD" and "PremiumV2
-  #disk_iops_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_write : null
-  #disk_mbps_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_write : null
-  #disk_iops_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_only : null
-  #disk_mbps_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_only : null
-  #logical_sector_size  = var.logical_sector_size
+  disk_iops_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_write : null
+  disk_mbps_read_write = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_write : null
+  disk_iops_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_iops_read_only : null
+  disk_mbps_read_only  = contains(["UltraSSD", "PremiumV2"], var.os_storage_account_type) ? var.disk_mbps_read_only : null
+  logical_sector_size  = var.logical_sector_size
 
   #edge_zone = ?
-
-  os_type = contains(["Import", "ImportSecure", "Copy"], var.os_type) ? var.os_type : null
 
   max_shares = var.maximum_shares
   zone       = var.vm_avail_zone_id
 
-  encryption_settings {
+  dynamic "encryption_settings" {
+    for_each = var.disk_encryption_secret != null ? ["true"] : var.key_encryption_key != null ? ["true"] : []
+    content {
 
-    dynamic "disk_encryption_key" {
-      for_each = var.disk_encryption_secret != null ? ["true"] : []
+      dynamic "disk_encryption_key" {
+        for_each = var.disk_encryption_secret != null ? ["true"] : []
 
-      content {
-        source_vault_id = var.disk_encryption_secret["source_vault_id"]
-        secret_url      = var.disk_encryption_secret["secret_url"]
+        content {
+          source_vault_id = var.disk_encryption_secret["source_vault_id"]
+          secret_url      = var.disk_encryption_secret["secret_url"]
+        }
       }
-    }
 
-    dynamic "key_encryption_key" {
-      for_each = var.key_encryption_key != null ? ["true"] : []
-      content {
-        source_vault_id = var.key_encryption_key["source_vault_id"]
-        key_url         = var.key_encryption_key["key_url"]
+      dynamic "key_encryption_key" {
+        for_each = var.key_encryption_key != null ? ["true"] : []
+        content {
+          source_vault_id = var.key_encryption_key["source_vault_id"]
+          key_url         = var.key_encryption_key["key_url"]
+        }
       }
     }
   }
 
   tags = merge(local.tags_default, var.tags)
+
+  lifecycle {
+
+    precondition {
+      condition     = (var.os_disk_size_gb == null)
+      error_message = "The variable os_disk_size_gb is REQUIRED, only for UltraSSD and PremiumV2 (os_Storage_account_type)"
+    }
+  }
+
 }
